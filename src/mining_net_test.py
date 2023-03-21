8e447200e1d825d1486e1d588c4ec55eb1ca2fb2
srijal30 <srijal30@stuy.edu>
2023-03-21 18:46:59 -0400

    worked on new mining interface

--- src/mining_net_test.py
+++ src/mining_net_test.py
@@ -8,38 +8,39 @@ from proto.schema_pb2 import *
 from crypto import *
 
 
-print("genesis done\n\n")   
+miner_pub_key = serialize_public_key(create_keys()[1])
+print("MINER:", miner_pub_key)
 users = [create_keys() for i in range(10)]
 
-def random_transaction(node):
+def random_transaction(node: MiningNode):
     sender, receiver = sample(users, 2)
     t = Transaction()
     t.sender_pub_key = serialize_public_key(sender[1])
     t.receiver_pub_key = serialize_public_key(receiver[1])
-    t.sequence = node.snapshot.accounts[t.sender_pub_key].sequence
+    t.sequence = node.uncommitted_snapshot.accounts[t.sender_pub_key].sequence
     t.amount = 0
     # t.amount = randint(1, snapshot.accounts[sender_pub])
     t.hash = hash_transaction(t)
     t.signature = create_signature(t.hash, sender[0])
     return t        
 
-
-
 def give_transactions(node: MiningNode):
     sleep(1)
     node.add_transaction(random_transaction(node))
 
-
 # START
-test = MiningNode()
+test = MiningNode(miner_pub_key)
 test.start()
 
 # adds transactions every second
-tran_sender= Thread(target=give_transactions, args=(test,))
-tran_sender.start()
+# tran_sender= Thread(target=give_transactions, args=(test,))
+# tran_sender.start()
 
-STOP_AT = 2
-while len(test.blockchain.blocks) != STOP_AT:
+max_blocks = 2
+cur_block = 1
+while len(test.blockchain.blocks) != max_blocks:
+    if len(test.blockchain.blocks) != cur_block:
+        cur_block += 1
     pass
-test.stop()
 
+print(test.blockchain)
--- src/networking.py
+++ src/networking.py
@@ -3,9 +3,14 @@ File that contains the interface for a mining node
 """
 
 from threading import Thread
-from crypto import hash_block
-from blockchain import calculate_difficulty, calculate_reward, replay_transaction, validate_transaction
-
+from crypto import hash_block, generate_merkle_root
+from blockchain import (
+    calculate_difficulty, 
+    calculate_reward, 
+    replay_transaction, 
+    validate_transaction,
+    add_block
+)
 from proto.schema_pb2_grpc import MiningNodeServicer
 from proto.schema_pb2 import (
     BlockChain,
@@ -15,45 +20,64 @@ from proto.schema_pb2 import (
     Snapshot
 )
 
+
+# TO DO: add a way to choose whether to start or join the network
 class MiningNode(MiningNodeServicer):
     """Implementation of a mining node"""
     def __init__(self, miner_pub_key: str):
         self.miner_pub_key = miner_pub_key
         self.transaction_pool = []
+        self.miner = MiningService(self)
         
         self.committed_snapshot = Snapshot()
         self.uncommitted_snapshot = Snapshot()
         
         self.blockchain = BlockChain()
-        self.genesis = Block(prev_hash="0"*64, merkle_root="0"*64, trans=[])
-        self.genesis.curr_hash = hash_block(self.genesis)
-        
-        self.miner = MiningService(self.mine_next_block)
-        
+        # TEMP: create a genesis block
+        # difficulty of genesis block
+        genesis = Block(prev_hash="0"*64, merkle_root="0"*64, trans=[])
+        genesis.curr_hash = hash_block(genesis)
+        add_block(self.committed_snapshot, genesis, self.blockchain)
+
+    def start(self) -> None:
+        """Starts the mining node."""
+        # join network...
+        # start mining
+        self.mine_next_block()
+
     def add_transaction(self, trans : Transaction) -> None:
-        """Adds transactions to pool after verifying and adding to uncommitted snapshot."""
+        """Adds transaction to the pool if it is valid."""
         if validate_transaction(trans) and replay_transaction(self.uncommitted_snapshot, trans):
             self.transaction_pool.append(trans) 
 
     def mine_next_block(self):
-        """Creates a new block to be mined"""
-        new_block = Block()
-        new_block.trans = self.transaction_pool.copy()
-        new_block.miner = self.miner_pub_key
-        new_block.difficulty = calculate_difficulty(self.blockchain)
-        new_block.mining_reward = calculate_reward(self.blockchain)
+        """Creates and starts mining a new block."""
+        # setup the new block
+        new_block = Block(
+            prev_hash=self.blockchain.blocks[-1].curr_hash,
+            trans=self.transaction_pool.copy(),
+            miner=self.miner_pub_key,
+            difficulty=calculate_difficulty(self.blockchain),
+            mining_reward=calculate_reward(self.blockchain),
+        )
+        generate_merkle_root(new_block)
+        self.miner.start_mining(new_block)
 
-
-    def start(self) -> None:
-        """Starts the mining node."""
-        # join network
-        # start mining
+    def add_block(self, block: Block):
+        """Will get called when new block needs to be added to the chain. 
+        Only will add the block after checking validity.
+        Will update snapshots and clean up transaction pool."""
+        # add the block
+        success = add_block(self.committed_snapshot, block, self.blockchain)
+        # clean up transactions
+        if success:
+            for tran in block.trans:
+                if tran in self.transaction_pool: # will this code even work? will this compare memory addresses?
+                    self.transaction_pool.remove(tran)
+                else: # if not in transaction pool, it means we have to add it to uncommited snapshot
+                    replay_transaction(self.uncommitted_snapshot, tran)
+        # start mining next block
         self.mine_next_block()
-        pass
-
-    def mining_finished() -> None:
-        """This will be called when the mining service finishes mining a block"""
-        pass
 
     # NETWORKING FUNCTIONS
     def send_blockchain(self, request, context):
@@ -72,26 +96,27 @@ class MiningNode(MiningNodeServicer):
 # should we make mining service spawn multiple threads to make mining faster
 class MiningService():
     """Service that mines blocks in the background."""
-    def __init__(self, callback: function):
+    def __init__(self, node: MiningNode):
         self.current_thread : Thread = None
         self.stopped = True
-        self.callback = callback
+        self.parent = node
 
-    def _mine(self, block: Block, difficulty: int) -> None: 
+    def _mine(self, block: Block) -> None: 
         """Mines blocks until signed hash is generated."""
         block.nonce = 0
         block.curr_hash = hash_block(block)
-        while block.curr_hash[0:difficulty] != "0"*difficulty:
-            if self.stopped:
+        while block.curr_hash[0:block.difficulty] != "0"*block.difficulty:
+            if self.stopped:  # stops without calling callback
                 return
             block.nonce += 1
             block.curr_hash = hash_block(block)
-        self.callback()
+        # since fully completes, callback can be called
+        self.parent.add_block(block)
 
-    def start_mining(self, block: Block, difficulty: int) -> None:
+    def start_mining(self, block: Block) -> None:
         """Creates thread for mining"""
         self.stopped = False
-        self.current_thread = Thread(target=self._mine, args=(block, difficulty,))
+        self.current_thread = Thread(target=self._mine, args=(block,))
         self.current_thread.start()
 
     def stop_mining(self):
