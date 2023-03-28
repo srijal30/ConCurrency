6a730869a750fbd59edea19ccc0a094498a38556
srijal30 <srijal30@stuy.edu>
2023-03-28 19:29:06 -0400

    added networking test

--- src/networking.py
+++ src/networking.py
@@ -5,7 +5,9 @@ File that contains the networking functions
 import sys
 sys.path.append("src/proto/")
 
-from proto.schema_pb2_grpc import NetworkServicer
+import grpc
+import concurrent.futures as futures
+from proto.schema_pb2_grpc import NetworkServicer, NetworkStub, add_NetworkServicer_to_server
 from proto.schema_pb2 import *
 from blockchain import *
 from crypto import *
@@ -26,15 +28,18 @@ class Network(NetworkServicer):
         latest_block_hash = self.blockchain.blocks[-1].curr_hash
         if request.block.prev_hash == latest_block_hash and validate_block(request.block):
             # add the block
-            self.blockchain.lock()
-            self.committed_snap.lock()
+
+            # self.blockchain.lock()
+            # self.committed_snap.lock()
             add_block(self.committed_snap, request.block, self.blockchain)
-            self.blockchain.unlock()
-            self.committed_snap.unlock()
+            # self.blockchain.unlock()
+            # self.committed_snap.unlock()
+            
             # tie loose ends with uncommited snapshot and transaction pool
             # THIS NEEDS REVIEW (also maybe add this functionality to datamodel)
-            self.transaction_pool.lock()
-            self.uncommitted_snap.lock()
+            
+            # self.transaction_pool.lock()
+            # self.uncommitted_snap.lock()
             for tran in request.block.trans:
                 if tran in self.transaction_pool: # will this code even work? will this compare memory addresses?
                     self.transaction_pool.remove(tran)
@@ -46,12 +51,12 @@ class Network(NetworkServicer):
     
     def send_transaction(self, request: SendTransactionRequest, context) -> SendTransactionReply:
         """Adds transaction to transaction pool if it is valid. Also updates uncommited snapshot."""
-        self.uncommitted_snap.lock()
+        # self.uncommitted_snap.lock()
         if validate_transaction(request.transaction) and replay_transaction(self.uncommitted_snap, request.transaction):
-            self.transaction_pool.lock()
+            # self.transaction_pool.lock()
             self.transaction_pool.append(request.transaction)
-            self.transaction_pool.unlock()
-        self.uncommitted_snap.unlock()
+            # self.transaction_pool.unlock()
+        # self.uncommitted_snap.unlock()
         return SendTransactionReply()    
 
     def get_block(self, request: GetBlockRequest, context) -> GetBlockReply:
@@ -74,12 +79,44 @@ class Network(NetworkServicer):
         return RequestTransactionReply()
 
 
-import threading
-def test_server():
-    pass
-
-def test_client():
-    pass
-
+# QUICK TEST THAT REQUESTS BLOCK FROM SERVER
 if __name__ == "__main__":
-    pass
+    def test_server():
+        # set up the test
+        blockchain = BlockChain()
+        test_block = Block(
+                curr_hash="1"*64,
+                prev_hash="0"*64,
+                miner_pub_key="Satoshi Nakamoto"  
+        )
+        blockchain.blocks.append(test_block)
+        pool : List[Transaction] = []
+        # starting the server
+        server = grpc.server(futures.ThreadPoolExecutor(max_workers=10))
+        service = Network(blockchain, Snapshot(), Snapshot(), pool)
+        add_NetworkServicer_to_server(service, server)
+        server.add_insecure_port("[::]:50001")
+        server.start()
+        server.wait_for_termination(timeout=3.0)
+        print("server terminated")
+
+    def test_client():
+        request = GetBlockRequest(
+            hash="1"*64
+        )
+        channel = grpc.insecure_channel('localhost:50001')
+        stub =  NetworkStub(channel)
+        received_block = stub.get_block(request)
+        print("\nclient received this from the server:\n", received_block)
+    
+    import threading
+    s = threading.Thread(target=test_server)
+    print("starting server...")
+    s.start()
+
+    c = threading.Thread(target=test_client)
+    print("\nstarting client and sending request...")
+    c.start()
+
+    c.join()
+    s.join()
