634db2ea6be5b38d1f697cf5e313c9993854279a
srijal30 <srijal30@stuy.edu>
2023-04-03 13:08:07 -0400

    refactoring loader.py

--- src/loader.py
+++ src/loader.py
@@ -1,10 +1,13 @@
 from proto.schema_pb2 import *
 
 def store_blockchain(chain: BlockChain, path: str) -> None:
+    """"""
     with open(path, 'wb') as file:
         file.write(chain.SerializeToString())
 
+
 def load_blockchain(path: str) -> BlockChain:
+    """"""
     try:
         with open(path, 'rb') as file:
             chain = BlockChain()
