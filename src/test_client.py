6b2794675c5a91594324af6672cac562ca197209
srijal30 <srijal30@stuy.edu>
2023-04-09 19:19:06 -0400

    made test_client/server work

--- src/test_client.py
+++ src/test_client.py
@@ -8,10 +8,13 @@ from model.proto.schema_pb2_grpc import *  # grpc is imported here
 
 if __name__ == "__main__":
     request = GetBlockRequest(
-            hash="0"*64
+            hash="1"*64
         )
     channel = grpc.insecure_channel('localhost:50001')
     stub =  NetworkStub(channel)
+
     received_block = stub.get_block(request)
+
+    print("here")
     print("\nclient received this from the server:\n", received_block)
     pass
--- src/test_server.py
+++ src/test_server.py
@@ -3,32 +3,36 @@ sys.path.append("src/model/proto/")
 
 import concurrent.futures as futures
 from typing import List
+import grpc
 
 from model.loader import load_blockchain, store_blockchain
-from model.proto.schema_pb2_grpc import *  # grpc is imported here
+from networking import Network
+from model.blockchain import TalkingStick
+from model.proto.schema_pb2_grpc import add_NetworkServicer_to_server
 from model.proto.schema_pb2 import *
 
 
 if __name__ == "__main__":
     try:
-        blockchain = load_blockchain("blockchain.data")
+        # set up the model
+        model = TalkingStick()
         test_block = Block(
                 curr_hash="1"*64,
                 prev_hash="0"*64,
                 miner_pub_key="Satoshi Nakamoto"  
         )
-        blockchain.blocks.append(test_block)
-        pool : List[Transaction] = []
-        # starting the server
+        model.blockchain.blocks.append(test_block)
+        print(model.blockchain)
+        
+        # start the server
         server = grpc.server(futures.ThreadPoolExecutor(max_workers=10))
-        #service = Network(blockchain, Snapshot(), Snapshot(), pool) 
-        print('here')
-        service = Network()
+        service = Network(model)
         add_NetworkServicer_to_server(service, server)
         server.add_insecure_port("[::]:50001")
         server.start()
         server.wait_for_termination()
+
     except:
         print("server died")
-        store_blockchain(blockchain, "blockchain.data")
+        store_blockchain(model.blockchain, "blockchain.data")
         raise
