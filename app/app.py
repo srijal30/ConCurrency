f751a45b3d758d8ac340166e54574514fe7188ae
srijal30 <srijal30@stuy.edu>
2023-05-08 16:36:16 -0400

    tmp app for testing

---
+++ app/app.py
@@ -0,0 +1,27 @@
+from typing import List
+from flask import Flask, request
+
+app = Flask(__name__)
+
+peer_list: List[str] = []
+
+@app.route("/api/get_nodes")
+def get_node_list():
+    return ["localhost"]
+
+@app.route("/api/connect")
+def connect_peer():
+    ip: str = request.remote_addr
+    if ip not in peer_list:
+        peer_list.append(ip)
+    return "success"
+
+@app.route("/api/disconnect")
+def disconnect_peer():
+    ip: str = request.remote_addr
+    if ip in peer_list:
+        peer_list.remove(ip)
+    return "success"
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0")
