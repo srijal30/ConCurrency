476d1d1fdc12d90145d9e6fbdb134a3978433501
srijal30 <srijal30@stuy.edu>
2023-05-16 19:37:26 -0400

    testing disconnect

--- app/app.py
+++ app/app.py
@@ -1,5 +1,33 @@
 from typing import List
+from networking import socket
 from flask import Flask, request
+from threading import threading
+import time
+
+#((TESTINGGGG)) Checks if port is in use, otherwise, single node network will try to send a request to an inactive port
+# probably want to get rid of this later, since we are going to be using a central server (rendzevous???)
+# note: only works for localhost!!!
+
+def is_port_in_use(ip: str, port: str) -> bool:
+    with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+        return s.connect_ex((ip + ":" + port)) == 0
+
+def peer_list_heartbeat() -> None:
+    for ip in peer_list:
+        if not (is_port_in_use(ip, "50001")):
+           peer_list.remove(ip)
+           print("removed ip")
+
+def pulse(start : bool) -> None:
+    while start:
+        if (int(time.time()/60000000000) % 5 == 0):
+            peer_list_heartbeat()
+
+
+
+t1 = threading.Thread(target=pulse(), args=(True))
+t1.start()
+
 
 app = Flask(__name__)
 
@@ -16,12 +44,8 @@ def connect_peer():
         peer_list.append(ip)
     return ip
 
-@app.route("/api/disconnect")
-def disconnect_peer():
-    ip: str = request.remote_addr
-    if ip in peer_list:
-        peer_list.remove(ip)
-    return ip
+# NOTE: THIS IS NOT IDEAL, CHANGE TO HEARBEAT
+
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0")
